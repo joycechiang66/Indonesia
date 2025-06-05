65e0d977d5eada8fce497211b8bbb740bd134fcc
joycechiang66 <joycechiang2008@gmail.com>
2025-06-06 00:01:13 +0800

    清理 public/assets 與圖片路徑調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
 import './styles/global.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Fira Sans',
+      'Droid Sans',
+      'Helvetica Neue',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
 function App() {
   return (
-    <Router basename="/Indonesia">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Add more routes here as needed */}
-        </Routes>
-      </Layout>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename="/Indonesia">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
